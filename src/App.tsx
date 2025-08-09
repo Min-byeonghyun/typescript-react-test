11dbec0208ac5334cc1b32fa5c69fb2c8a3e8b2d
Min-byeonghyun <qudgus5125@naver.com>
2025-08-09 22:01:42 +0900

    상태관리 props type지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import "./App.css";
+import { useState, useRef, useEffect } from "react";
+import Editor from "./components/Editor";
+
+interface Todo {
+  id: number;
+  content: string;
+}
 
 function App() {
-  return <div className="App"></div>;
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([...todos, { id: idRef.current++, content: text }]);
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+  return (
+    <div className="App">
+      <h1>Test</h1>
+      <Editor onClickAdd={onClickAdd} />
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id}> {todo.content}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
